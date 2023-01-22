3b236a34c493aa1711f1ca266dd780ae6ee25026
Bashar Alwarad <b@gmail.com>
2023-01-22 09:42:58 +0100

    create reouter & AboutPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
 
 import FeedbackData from './data/FeedbackData'
 
@@ -26,12 +29,29 @@ function App() {
   }
 
   return (
-    <div>
-      <Header text={text && text} style={style && style} />
-      <FeedbackForm handelAdd={addFeedback} />
-      <FeedbackStats feedback={feedback} />
-      <FeedbackList feedback={feedback} handelDelete={deleteFeedback} />
-    </div>
+    <Router>
+      <Header />
+      <div className='container'>
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <>
+                <FeedbackForm handelAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handelDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
+      </div>
+    </Router>
   )
 }
 
